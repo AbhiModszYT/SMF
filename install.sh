d500175981bc6bf86416637f8038c8c91aa5f61d
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-06-06 17:32:25 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#code_recorded_by_AM_BOT
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+   ___   __  ______  ____  ______  
+  / _ | /  |/  / _ )/ __ \/_  __/  
+ / __ |/ /|_/ / _  / /_/ / / /     
+/_/ |_/_/  /_/____/\____/ /_/      
+                                   " | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip am.zip
+clear
+rm -rf am.zip
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+echo " Join MY CHANNEL 🔔 " | lolcat
+xdg-open https://t.me/AbhiModszYT_Return
+echo ""
+sleep 10
+echo " create account on Telegram.org "
+echo " set your browser "
+termux-open-url https://t.me/AbhiModszYT_Return
+echo ""
